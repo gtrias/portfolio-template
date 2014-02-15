668fa657202cb49a5d22611477a4b9c4dde4b04c
root <root@web.canparres.net>
2014-02-15 17:17:07 +0100

    Added missing files

--- functions.php
+++ functions.php
@@ -126,7 +126,7 @@ function portfolio_has_featured_posts() {
  */
 function portfolio_widgets_init() {
 	require get_template_directory() . '/inc/widgets.php';
-	register_widget( 'Twenty_Fourteen_Ephemera_Widget' );
+	//register_widget( 'Twenty_Fourteen_Ephemera_Widget' );
 
 	register_sidebar( array(
 		'name'          => __( 'Primary Sidebar', 'portfolio' ),
